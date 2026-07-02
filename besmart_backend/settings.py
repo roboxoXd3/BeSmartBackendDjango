63a943f839eea83a409af6a7c7c1fcce730f125d
Avi <sa3198154@gmail.com>
2026-07-02 16:15:49 +0530

    fix: register SupabaseAuthentication in DRF settings to resolve 401 on auth endpoints

--- besmart_backend/settings.py
+++ besmart_backend/settings.py
@@ -224,6 +224,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'users.authentication.SupabaseAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ),
