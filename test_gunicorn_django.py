cadad35b3a2aa5c405ec3b7603d9cf0e277a8718
Avi <sa3198154@gmail.com>
2026-07-08 11:18:40 +0530

    chore: remove test scripts, add migrations and update gitignore

--- test_gunicorn_django.py
+++
@@ -1,8 +0,0 @@
-from django.core.wsgi import get_wsgi_application
-import os
-import threading
-import django
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'besmart_backend.settings')
-print(f"Loading Django app in PID: {os.getpid()}, Threads: {threading.active_count()}")
-application = get_wsgi_application()
--- test_structlog.py
+++
@@ -1,9 +0,0 @@
-import logging
-from besmart_backend.settings import LOGGING
-import logging.config
-
-logging.config.dictConfig(LOGGING)
-
-logger = logging.getLogger("django")
-logger.info("Test log from standard python logger")
-
